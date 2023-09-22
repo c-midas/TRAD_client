8b7f8812bacf250e37af13954d76da180f0c9839
c-midas <midashand99@gmail.com>
2023-09-22 08:50:45 -0700

    fix: lint err

--- src/screens/Application/redux/ApplicationAction.js
+++ src/screens/Application/redux/ApplicationAction.js
@@ -628,7 +628,7 @@ export const viewApplicationUploadDocuments = (dataArr, config, callBack) => {
         }
         const len = dataArr.length;
 
-        for  (let i = 0; i < len; i++) {
+        for (let i = 0; i < len; i+=1) {
           if (resArr[i]?.data?.status === 'SUCCESS') {
             dispatch({
               type: APPLICATION_REDUX_CONSTANTS.VIEW_APPLICATION.APPLICATION_MODULES
